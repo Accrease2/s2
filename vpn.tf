6416565459490b75f7ca026681fcff3385e9ae50
Accrease2 <carnostf2@gmail.com>
2024-11-14 08:29:10 +0700

    Update vpn.tf

--- vpn.tf
+++ vpn.tf
@@ -62,7 +62,7 @@ resource "aws_route_table" "private_route_table" {
 
 # NAT GATEWAY
 resource "aws_route" "private_nat_gateway" {
-  route_table_id         = "${aws_route_table.private.id}"
+  route_table_id         = "${aws_route_table.private_route_table.id}"
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id         = "${aws_nat_gateway.nat.id}"
 }
